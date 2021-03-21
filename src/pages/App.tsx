4eebe804233d7f369ff8cc3552189b2a5ae0a236
Fahad Ahmed <contact@fhdamd.dev>
2021-03-22 01:22:39 +1100

    testing husky

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -5,7 +5,7 @@ import TwitterImg from '../images/twitter-brands.svg';
 function App(): JSX.Element {
   return (
     <>
-      <Header>Hello React 2021</Header>
+      <Header>                            Hello React 2021<                 /Header>
       <img src={TwitterImg} alt="Twitter" width="50" height="50" />
       <p className="test-class">This is a paragraph implementing global styles.</p>
     </>
